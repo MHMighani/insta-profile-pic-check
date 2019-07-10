b8ada539d58d5a3379b0550320d3d18903ecddae
Hermes223 <mh.mgh2012@gmail.com>
2019-07-10 17:04:18 +0430

    adding modified main.py

--- main.py
+++ main.py
@@ -6,15 +6,18 @@ menuItems = [
     {'Check all users': option_three},
     {'Delete a User': option_four},
     {'See a user\'s profile pic': option_five},
+    {'to see all bio\'s': option_six},
     {'Exit' : exit}
-] 
+]
 
 def main():
-    if("dic.pickle" not in os.listdir()):
+    if "dic.pickle" not in os.listdir():
         dic = {}
         pickle_file_dump("dic.pickle",dic)
+    if "dic2.pickle" not in os.listdir():
+        dic2 = {}
+        pickle_file_dump("dic2.pickle",dic2)
 
-    
     clear_screen()
     fig = Figlet(font='doom')
     print(fig.renderText("profile check"))
